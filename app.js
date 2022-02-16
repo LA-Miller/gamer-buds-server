fe1c87415f729a08a7a5d2397aa31b8b20fe2244
Luke Miller <lamiller516@gmail.com>
2022-02-16 18:49:06 -0500

    removed force: true

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(require("./middleware/validate-jwt"));
 
 // Changes made
 dbConnection.authenticate()
-    .then(() => dbConnection.sync({ force: true }))
+    .then(() => dbConnection.sync())
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log(`[Server]: App is listening on ${process.env.PORT}.`);
